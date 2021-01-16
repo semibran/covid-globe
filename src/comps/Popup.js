f8e0f64b0d0162bd9508b2833dc1e2550ecc1277
Joshua Krauchi <jkrauchi@my.bcit.ca>
2021-01-16 13:01:50 -0800

    Sidebar changes

--- src/comps/Popup.js
+++ src/comps/Popup.js
@@ -8,9 +8,22 @@ Popup.propTypes = {
 export default function Popup (props) {
   return (
   <div className='popup'>
-    <img src='assets/round.png'/>
-    <h2>STATISTICS + CONTROLS</h2>
-    <span onClick={props.onClose} className='material-icons-round'>close</span>
+    <div id='popupTitle'>
+      <img src='assets/round.png'/>
+      <h2>STATISTICS + CONTROLS</h2>
+      <button onClick={props.onClose} className='material-icons-round'>close</button>
+    </div>
+    <h3>COUNTRY STATISTICS</h3>
+    popupmenu
+    <div id='popupStats'></div>
+    <h3 id='date'></h3>
+    <div id='popupData'>
+      <span id='cases'>Active Cases:</span>
+      <span id='totalCases'>Total Cases to Date:</span>
+      <span id='totalDeaths'>Total Deaths to Date:</span>
+    </div>
+    <h3>Controls</h3>
+    <div id='controls'></div>
   </div>
   );
 }
