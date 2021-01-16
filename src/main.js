5231d54fff1bf7d8422e682a0a58bbba7e77bbb3
Joshua Krauchi <jkrauchi@my.bcit.ca>
2021-01-16 11:12:53 -0800

    Globe update

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import * as THREE from 'three'
 import ThreeGlobe from 'three-globe'
 import TrackballControls from 'three-trackballcontrols'
+
 import data from './data/countries.json'
 import covid from './data/covid.json'
+
 const renderer = new THREE.WebGLRenderer({
   alpha: true,
   // TO CHANGE
@@ -22,17 +24,15 @@ var x = Math.floor(Math.random() * 255);
 const globe = new ThreeGlobe()
   .globeImageUrl('//unpkg.com/three-globe/example/img/earth-water.png')
   .polygonsData(data.features)
-  .polygonCapColor((countries) => {
-    /*
-    console.log(countries);
-    for (i = 0; i < 255; i++) {
-      for (j = 0; j < 255; j++) {
-        if (countries.features[i].properties.ISO_A3 == "CAN") {
+  .polygonCapColor((country) => {
+    for (i = 0; i < covid.length; i++) {
+      if (country.properties.ISO_A3 == covid[i].ISO_A3) {
+        let intensity = covid[i].intensity;
+        if (intensity) {
           return 'rgba(0, 255, 0, 1)';
         }
       }
     }
-    */
     return 'rgba(255, 0, 0, 1)';
   })
   .polygonSideColor(() => 'rgba(0, 0, 0, 0)')
@@ -46,8 +46,8 @@ const globe = new ThreeGlobe()
     globeMaterial.bumpScale = 10;
     new THREE.TextureLoader().load('//unpkg.com/three-globe/example/img/earth-water.png', texture => {
       globeMaterial.specularMap = texture;
-      globeMaterial.specular = new THREE.Color('grey');
-      globeMaterial.shininess = 25;
+      globeMaterial.specular = new THREE.Color('white');
+      
     });
 
 //setTimeout(() => globe.polygonAltitude(() => Math.random()), 4000)
@@ -55,7 +55,7 @@ const globe = new ThreeGlobe()
 // Set up scene
 const scene = new THREE.Scene()
 scene.add(globe)
-scene.add(new THREE.AmbientLight(0xdddddd))
+scene.add(new THREE.AmbientLight(0xffffff))
 //scene.add(new THREE.DirectionalLight(0xffffff, 0.6))
 
 // Set up camera controls
