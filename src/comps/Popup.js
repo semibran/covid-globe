46425cba331fc225021ec0616a4cd81add05b7f3
Joshua Krauchi <jkrauchi@my.bcit.ca>
2021-01-16 12:36:10 -0800

    Sidebar

--- src/comps/Popup.js
+++ src/comps/Popup.js
@@ -6,8 +6,11 @@ Popup.propTypes = {
 }
 
 export default function Popup (props) {
-  return <div className='popup'>
-    <p>Hello, I&apos;m a popup!</p>
+  return (
+  <div className='popup'>
+    <img src='assets/round.png'/>
+    <h2>STATISTICS + CONTROLS</h2>
     <span onClick={props.onClose} className='material-icons-round'>close</span>
   </div>
+  );
 }
